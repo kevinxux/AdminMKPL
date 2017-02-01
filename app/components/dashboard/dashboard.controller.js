49ddb797f037f529dc7d70222b289c82c7641a8a
Renato Centeno <hello@renatocenteno.com>
2017-02-01 00:23:58 -0500

    Dashboard template

--- app/components/dashboard/dashboard.controller.js
+++ app/components/dashboard/dashboard.controller.js
@@ -9,8 +9,8 @@
     function DashboardController(Util, $scope) {
         var vm = this;
         Util.active('dashboard');
-        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-		$scope.series = ['Series A', 'Series B'];
+        $scope.labels = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio"];
+		$scope.series = ['Usuario Tipo A', 'Usuario Tipo B'];
 		$scope.data = [
 			[65, 59, 80, 81, 56, 55, 40],
 			[28, 48, 40, 19, 86, 27, 90]
